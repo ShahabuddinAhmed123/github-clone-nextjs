3a0c6690dce6da32031c36e96d919ed850bdb8a6
ShahabuddinAhmed123 <shahabtanolo123@gmail.com>
2025-03-02 02:06:02 +0500

    added navbar components and started making responsive

--- components/SectionFive.tsx
+++ components/SectionFive.tsx
@@ -81,7 +81,7 @@ export default function SectionFive() {
               left:
                 Object.keys(accordionData.categories).indexOf(activeCategory) *
                   140 +
-                80,
+                60,
             }}
             transition={{ type: "spring", stiffness: 150, damping: 20 }}
             style={{
